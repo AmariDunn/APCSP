69b9f2dfc1f963d9ea3a08487950353b1779f568
142037 <142037@hs-b101-05.district.bcs>
2022-08-26 08:11:05 -0500

    Unit 1.

---
+++ src/main/java/Exercise1_1.java
@@ -0,0 +1,13 @@
+/*
+Amari Dunn
+Exercise 1.1
+8/25/22
+Display to Console
+ */
+
+public class Exercise1_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
