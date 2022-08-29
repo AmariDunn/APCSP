ee03475dcbbdf4f8410d3e520c7655b2303370b1
142037 <142037@hs-b101-05.district.bcs>
2022-08-29 08:25:10 -0500

    Unit 1.

--- src/main/java/Exercise1_5.java
+++ src/main/java/Exercise1_5.java
@@ -4,6 +4,11 @@ Amari
 Exercise 1.5
  */
 
+/**
+ * 
+ * @author 142037
+ */
+
 public class Exercise1_5 {
     public static void main(String[] args) {
         System.out.println(" (9.5 x 4.5 - 2.5 x 3 ) / (45.5 - 3.5) = " + (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5 ) ) ;
