b9ca204d4fc86fe4dc76c08b9162fae2c9b348e2
142037 <142037@hs-b101-05.district.bcs>
2022-08-29 08:53:18 -0500

    Unit 1.

--- src/main/java/Exercise2_1.java
+++ src/main/java/Exercise2_1.java
@@ -2,12 +2,31 @@
 Amari
 8/26/22
 Exercise 2.1
+Convert Celcius to Fahrenheit
  */
 /**
  * 
  * @author 142037
  */
 
+import java.util.Scanner;
+
 public class Exercise2_1 {
-    
+    public static void main (String[] args) {
+        //Scanner Object
+        Scanner input = new Scanner (System.in);
+        
+        //User Input
+        System.out.println( "Enter a degree in Celsius" );
+        
+        //Remember that order matters!
+        
+        //Variables
+        double celcius = input.nextDouble();
+        double fahrenheit = (9.0/5) * celcius + 32;
+        
+        //Display Result
+        System.out.println(celcius + " Celcius is " + fahrenheit + " Fahrenheit");
+    }
+            
 }
