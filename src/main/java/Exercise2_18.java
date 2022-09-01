12a85f00beaf32ee52c60308da1e7dbf04db6e22
142037 <142037@hs-b101-05.district.bcs>
2022-09-01 08:43:21 -0500

    Unit 1.

---
+++ src/main/java/Exercise2_18.java
@@ -0,0 +1,22 @@
+/*
+Amari Dunn
+9/1/22
+Exercise 2.18
+Print a Table
+ */
+
+/**
+ *
+ * @author 142037
+ */
+
+public class Exercise2_18{
+    public static void main(String[] args){
+        System.out.println("a   b   pow(a, b)");
+        System.out.println("1   2   " + (int)(Math.pow(1, 2)) );
+        System.out.println("2   3   " + (int)(Math.pow(2, 3)) );
+        System.out.println("3   4   " + (int)(Math.pow(3, 4)) );
+        System.out.println("4   5   " + (int)(Math.pow(4, 5)) );
+        System.out.println("5   6   " + (int)(Math.pow(5, 6)) );
+    }
+}
