10aea6b9d94fa99b99bb4b1e78ca6c016b3dd944
142037 <142037@hs-b101-05.district.bcs>
2022-09-02 08:41:58 -0500

    Unit 1.

---
+++ src/main/java/Exercise2_7.java
@@ -0,0 +1,29 @@
+/*
+ Amari Dunn
+9/2/22
+Exercise 2_7
+Display Years and days
+ */
+
+/**
+ *
+ * @author 142037
+ */
+import java.util.Scanner;
+
+public class Exercise2_7 {
+    public static void main (String[] args){
+        Scanner input = new Scanner (System.in);
+        System.out.println("Enter the nubmer of minutes: ");
+        int minutes = input.nextInt();
+        int remainingMinutes = minutes;
+        
+        int years = (remainingMinutes / 525600) ;
+        remainingMinutes = remainingMinutes % 525600;
+        
+        int days = remainingMinutes / 1440;
+        
+        System.out.println( minutes + " minutes is approximately " + years + " years and " + days + " days" );
+        
+    }
+}
